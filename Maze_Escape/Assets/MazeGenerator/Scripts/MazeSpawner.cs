55b631da5773b603edb576a1658b88d79edef072
Mostafa-Usama <mostafa.usama286@gmail.com>
2022-12-17 02:19:17 +0200

    Uploaded

---
+++ Maze_Escape/Assets/MazeGenerator/Scripts/MazeSpawner.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//<summary>
+//Game object, that creates maze and instantiates it in scene
+//</summary>
+public class MazeSpawner : MonoBehaviour {
+	
+
+	public bool FullRandom = false;
+	int RandomSeed = 12345;
+	public GameObject Floor = null;
+	public GameObject Wall = null;
+	public GameObject Pillar = null;
+	public int Rows = 5;
+	public int Columns = 5;
+	 float CellWidth = 5;
+	 float CellHeight = 5;
+	
+	public GameObject GoalPrefab = null;
+
+	private BasicMazeGenerator mMazeGenerator = null;
+
+    [System.Obsolete]
+    void Start () {
+		if (!FullRandom) {
+			Random.seed = RandomSeed;
+		}
+		mMazeGenerator = new RecursiveTreeMazeGenerator (Rows, Columns);
+		mMazeGenerator.GenerateMaze ();
+		for (int row = 0; row < Rows; row++) {
+			for(int column = 0; column < Columns; column++){
+				float x = column*(CellWidth);
+				float z = row*(CellHeight);
+				MazeCell cell = mMazeGenerator.GetMazeCell(row,column);
+				GameObject tmp;
+				tmp = Instantiate(Floor,new Vector3(x,0,z), Quaternion.Euler(0,0,0)) as GameObject;
+				tmp.transform.parent = transform;
+				if(cell.WallRight){
+					tmp = Instantiate(Wall,new Vector3(x+CellWidth/2,0,z)+Wall.transform.position,Quaternion.Euler(0,90,0)) as GameObject;// right
+					tmp.transform.parent = transform;
+				}
+				if(cell.WallFront){
+					tmp = Instantiate(Wall,new Vector3(x,0,z+CellHeight/2)+Wall.transform.position,Quaternion.Euler(0,0,0)) as GameObject;// front
+					tmp.transform.parent = transform;
+				}
+				if(cell.WallLeft){
+					tmp = Instantiate(Wall,new Vector3(x-CellWidth/2,0,z)+Wall.transform.position,Quaternion.Euler(0,270,0)) as GameObject;// left
+					tmp.transform.parent = transform;
+				}
+				if(cell.WallBack){
+					tmp = Instantiate(Wall,new Vector3(x,0,z-CellHeight/2)+Wall.transform.position,Quaternion.Euler(0,180,0)) as GameObject;// back
+					tmp.transform.parent = transform;
+				}
+				if(cell.IsGoal && GoalPrefab != null && row != 0 && column != 0){
+					tmp = Instantiate(GoalPrefab,new Vector3(x,1,z), Quaternion.Euler(0,0,0)) as GameObject;
+					tmp.transform.parent = transform;
+				}
+			}
+		}
+		if(Pillar != null){
+			for (int row = 0; row < Rows+1; row++) {
+				for (int column = 0; column < Columns+1; column++) {
+					float x = column*(CellWidth);
+					float z = row*(CellHeight);
+					GameObject tmp = Instantiate(Pillar,new Vector3(x-CellWidth/2,0,z-CellHeight/2),Quaternion.identity) as GameObject;
+					tmp.transform.parent = transform;
+				}
+			}
+		}
+	}
+}
